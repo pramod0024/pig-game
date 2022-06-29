5c99fedad0407ae956ce2f69f4feb1c52b781b88
Pramod Jadhav <91459539+pramod0024@users.noreply.github.com>
2022-06-29 18:16:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ btnHold.addEventListener('click',function () {
     scores[activePlayer]= scores[activePlayer] + currentScore;
     document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
 
-    if(scores[activePlayer] >= 20){
+    if(scores[activePlayer] >= 50){
         playing= false;
         document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
         document.querySelector(`.player--${activePlayer}`).classList.remove('player--active')
